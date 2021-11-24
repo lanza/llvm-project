d259e2c1e0ceb6ba1e509d7dd693c76712a6fe2a
Nathan Lanza <nathan@lanza.io>
2021-11-23 21:09:28 -0500

    [DO NOT LAND] Revert "LoopOpt tutorial"
    This reverts commit 43057bcbb178b2975569ddfbc86d1a39a187c88c.

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -219,7 +219,6 @@
 #include "llvm/Transforms/Scalar/LoopInstSimplify.h"
 #include "llvm/Transforms/Scalar/LoopInterchange.h"
 #include "llvm/Transforms/Scalar/LoopLoadElimination.h"
-#include "llvm/Transforms/Scalar/LoopOptTutorial.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 #include "llvm/Transforms/Scalar/LoopPredication.h"
 #include "llvm/Transforms/Scalar/LoopRotation.h"
