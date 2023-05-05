ba403e0f4940c53c6af07a8fa8f664bac1e66ab7
Nathan Lanza <nathanlanza@gmail.com>
2023-05-05 01:52:18 -0400

    [nl] Fix more clangtidy complaints

--- llvm/tools/nl/Passes.cpp
+++ llvm/tools/nl/Passes.cpp
@@ -9,21 +9,33 @@ using namespace llvm;
 namespace nl {
 class ModulePass {
 public:
-  ModulePass() {}
+  ModulePass() = default;
   virtual void run(Module &module) = 0;
-  virtual ~ModulePass() {}
+  virtual ~ModulePass() = default;
+  ModulePass(ModulePass const &) = delete;
+  ModulePass(ModulePass &&) = default;
+  auto operator=(ModulePass const &) -> ModulePass & = delete;
+  auto operator=(ModulePass &&) -> ModulePass & = default;
 };
 class FunctionPass {
 public:
-  FunctionPass() {}
+  FunctionPass() = default;
   virtual void run(Function &function) = 0;
-  virtual ~FunctionPass() {}
+  virtual ~FunctionPass() = default;
+  FunctionPass(FunctionPass const &) = delete;
+  FunctionPass(FunctionPass &&) = default;
+  auto operator=(FunctionPass const &) -> FunctionPass & = delete;
+  auto operator=(FunctionPass &&) -> FunctionPass & = default;
 };
 class BasicBlockPass {
 public:
-  BasicBlockPass() {}
+  BasicBlockPass() = default;
   virtual void run(BasicBlock &block) = 0;
-  virtual ~BasicBlockPass() {}
+  virtual ~BasicBlockPass() = default;
+  BasicBlockPass(BasicBlockPass const &) = delete;
+  BasicBlockPass(BasicBlockPass &&) = default;
+  auto operator=(BasicBlockPass const &) -> BasicBlockPass & = delete;
+  auto operator=(BasicBlockPass &&) -> BasicBlockPass & = default;
 };
 class BasicBlockPassManager : public FunctionPass {
   std::vector<std::unique_ptr<BasicBlockPass>> passes;
@@ -35,7 +47,7 @@ public:
   template <typename T> void add(T pass) {
     passes.push_back(std::make_unique<T>(std::move(pass)));
   }
-  virtual void run(Function &function) override {
+  void run(Function &function) override {
     for (auto &pass : passes)
       for (auto &block : function)
         pass->run(block);
@@ -51,7 +63,7 @@ public:
   template <typename T> void add(T pass) {
     passes.push_back(std::make_unique<T>(std::move(pass)));
   }
-  virtual void run(Module &module) override {
+  void run(Module &module) override {
     for (auto &pass : passes)
       for (auto &function : module)
         pass->run(function);
@@ -62,9 +74,9 @@ class BasicBlockToModuleProxyPass : public ModulePass {
 
 public:
   template <typename T>
-  BasicBlockToModuleProxyPass(T pass)
+  explicit BasicBlockToModuleProxyPass(T pass)
       : pass{std::make_unique<T>(std::move(pass))} {}
-  virtual void run(Module &module) override {
+  void run(Module &module) override {
     for (auto &function : module)
       for (auto &block : function)
         pass->run(block);
@@ -83,7 +95,7 @@ public:
   template <typename T> void add(T pass) {
     passes.push_back(std::make_unique<T>(std::move(pass)));
   }
-  virtual void run(Module &module) override {
+  void run(Module &module) override {
     for (auto &pass : passes)
       pass->run(module);
   }
@@ -91,20 +103,20 @@ public:
 
 class TriviallyFoldConstantAddPass : public BasicBlockPass {
 public:
-  virtual void run(BasicBlock &block) override {
+  void run(BasicBlock &block) override {
     for (auto &insn : block) {
-      auto *op = dyn_cast<BinaryOperator>(&insn);
-      if (!op)
+      auto *binOp = dyn_cast<BinaryOperator>(&insn);
+      if (binOp == nullptr)
         continue;
-      if (op->getOpcode() != llvm::Instruction::Add)
+      if (binOp->getOpcode() != llvm::Instruction::Add)
         continue;
-      auto *lhs = op->getOperand(0);
+      auto *lhs = binOp->getOperand(0);
       auto *lhsConstant = dyn_cast<ConstantInt>(lhs);
-      if (!lhsConstant)
+      if (lhsConstant == nullptr)
         continue;
-      auto *rhs = op->getOperand(1);
+      auto *rhs = binOp->getOperand(1);
       auto *rhsConstant = dyn_cast<ConstantInt>(rhs);
-      if (!rhsConstant)
+      if (rhsConstant == nullptr)
         continue;
       // Probably doesn't do the right thing with regards to wrapping
       auto *replace = ConstantInt::get(
@@ -116,15 +128,15 @@ public:
 
 class TriviallyFoldAddZeroPass : public BasicBlockPass {
 public:
-  virtual void run(BasicBlock &block) override {
+  void run(BasicBlock &block) override {
     for (auto &insn : block) {
-      auto *op = dyn_cast<BinaryOperator>(&insn);
-      if (!op)
+      auto *binOp = dyn_cast<BinaryOperator>(&insn);
+      if (binOp == nullptr)
         continue;
-      if (op->getOpcode() != llvm::Instruction::Add)
+      if (binOp->getOpcode() != llvm::Instruction::Add)
         continue;
-      auto *lhs = op->getOperand(0);
-      auto *rhs = op->getOperand(1);
+      auto *lhs = binOp->getOperand(0);
+      auto *rhs = binOp->getOperand(1);
 
       if (auto *lhsConstant = dyn_cast<ConstantInt>(lhs)) {
         if (lhsConstant->getValue().isZero()) {
@@ -145,9 +157,9 @@ public:
 
 class RemoveDeadInstructionPass : public BasicBlockPass {
 public:
-  virtual void run(BasicBlock &block) override {
+  void run(BasicBlock &block) override {
     for (auto &insn : make_early_inc_range(block)) {
-      // TODO: is this right?
+      // TODO(lanza): is this right?
       if (insn.isTerminator())
         continue;
       if (insn.users().begin() == insn.users().end())
@@ -158,10 +170,7 @@ public:
 
 class PrintInstructionPass : public ModulePass {
 public:
-  PrintInstructionPass() {}
-  virtual ~PrintInstructionPass() {}
-
-  virtual void run(Module &module) override {
+  void run(Module &module) override {
     for (auto &function : module)
       for (auto &block : function)
         for (auto &insn : block)
@@ -170,7 +179,8 @@ public:
 };
 } // namespace nl
 
-std::vector<std::string_view> splitPasses(std::string_view passPipeline) {
+auto splitPasses(std::string_view passPipeline)
+    -> std::vector<std::string_view> {
 
   if (passPipeline.empty())
     return {passPipeline};
@@ -197,20 +207,20 @@ std::vector<std::string_view> splitPasses(std::string_view passPipeline) {
   return elements;
 }
 
-std::vector<std::unique_ptr<nl::ModulePass>>
-mapStringToPasses(std::vector<std::string_view> passes) {
+auto mapStringToPasses(const std::vector<std::string_view> &passes)
+    -> std::vector<std::unique_ptr<nl::ModulePass>> {
   using namespace std::literals;
   std::vector<std::unique_ptr<nl::ModulePass>> outPasses;
   for (auto passName : passes) {
     if (passName == "foldaddzero")
-      outPasses.emplace_back(
-          new nl::BasicBlockToModuleProxyPass(nl::TriviallyFoldAddZeroPass()));
+      outPasses.emplace_back(std::make_unique<nl::BasicBlockToModuleProxyPass>(
+          nl::TriviallyFoldAddZeroPass()));
     else if (passName == "foldaddconstant")
-      outPasses.emplace_back(new nl::BasicBlockToModuleProxyPass(
+      outPasses.emplace_back(std::make_unique<nl::BasicBlockToModuleProxyPass>(
           nl::TriviallyFoldConstantAddPass()));
     else if (passName == "deadinsn")
-      outPasses.emplace_back(
-          new nl::BasicBlockToModuleProxyPass(nl::RemoveDeadInstructionPass()));
+      outPasses.emplace_back(std::make_unique<nl::BasicBlockToModuleProxyPass>(
+          nl::RemoveDeadInstructionPass()));
     else
       llvm_unreachable("NYI");
   }
