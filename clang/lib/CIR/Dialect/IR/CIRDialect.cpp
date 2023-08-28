14f8d94bbd1c0b0c0cacf0c17d8e8e90526e505a
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-08-28 20:45:13 -0300

    [CIR] Refactor StructType printing/parsing
    This change simplifies the StructType printing/parsing methods. It also
    updates the representations to group members between braces and to
    remove commas between type attributes (e.g. id, packed, etc.).
    
    ghstack-source-id: 094d5064f827efe8963855ab8ffd04f4ad1cfb2b
    Pull Request resolved: https://github.com/llvm/clangir/pull/226

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -14,6 +14,7 @@
 #include "clang/CIR/Dialect/IR/CIRAttrs.h"
 #include "clang/CIR/Dialect/IR/CIROpsEnums.h"
 #include "clang/CIR/Dialect/IR/CIRTypes.h"
+#include <optional>
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMTypes.h"
@@ -2110,9 +2111,10 @@ void SignedOverflowBehaviorAttr::print(::mlir::AsmPrinter &printer) const {
 
 ::mlir::Attribute ASTFunctionDeclAttr::parse(::mlir::AsmParser &parser,
                                              ::mlir::Type type) {
-  // We cannot really parse anything AST related at this point
-  // since we have no serialization/JSON story.
-  return ASTFunctionDeclAttr::get(parser.getContext(), nullptr);
+  // We cannot really parse anything AST related at this point since we have no
+  // serialization/JSON story. Even if the attr is parsed, it just holds nullptr
+  // instead of the AST node.
+  return get(parser.getContext(), nullptr);
 }
 
 void ASTFunctionDeclAttr::print(::mlir::AsmPrinter &printer) const {
@@ -2127,9 +2129,10 @@ LogicalResult ASTFunctionDeclAttr::verify(
 
 ::mlir::Attribute ASTVarDeclAttr::parse(::mlir::AsmParser &parser,
                                         ::mlir::Type type) {
-  // We cannot really parse anything AST related at this point
-  // since we have no serialization/JSON story.
-  return ASTVarDeclAttr::get(parser.getContext(), nullptr);
+  // We cannot really parse anything AST related at this point since we have no
+  // serialization/JSON story. Even if the attr is parsed, it just holds nullptr
+  // instead of the AST node.
+  return get(parser.getContext(), nullptr);
 }
 
 void ASTVarDeclAttr::print(::mlir::AsmPrinter &printer) const {
@@ -2144,9 +2147,10 @@ LogicalResult ASTVarDeclAttr::verify(
 
 ::mlir::Attribute ASTRecordDeclAttr::parse(::mlir::AsmParser &parser,
                                            ::mlir::Type type) {
-  // We cannot really parse anything AST related at this point
-  // since we have no serialization/JSON story.
-  return ASTRecordDeclAttr::get(parser.getContext(), nullptr);
+  // We cannot really parse anything AST related at this point since we have no
+  // serialization/JSON story. Even if the attr is parsed, it just holds nullptr
+  // instead of the AST node.
+  return get(parser.getContext(), nullptr);
 }
 
 void ASTRecordDeclAttr::print(::mlir::AsmPrinter &printer) const {
