f41e4106705427f9acd1a7406d86626180ef1208
Nathan Lanza <nathanlanza@gmail.com>
2024-02-21 21:28:03 -0500

    [CIR] Create gatorade binary
    This is just copied from cir-opt

---
+++ clang/tools/gatorade/gatorade.cpp
@@ -0,0 +1,58 @@
+//===- gatorade.cpp - Cross-Library optimization tool -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// TODO
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Conversion/ReconcileUnrealizedCasts/ReconcileUnrealizedCasts.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/DLTI/DLTI.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/OpenMP/OpenMPDialect.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/Passes.h"
+#include "clang/CIR/Passes.h"
+
+int main(int argc, char **argv) {
+  // TODO: register needed MLIR passes for CIR?
+  mlir::DialectRegistry registry;
+  registry.insert<mlir::BuiltinDialect, mlir::arith::ArithDialect,
+                  mlir::cir::CIRDialect, mlir::memref::MemRefDialect,
+                  mlir::LLVM::LLVMDialect, mlir::DLTIDialect,
+                  mlir::omp::OpenMPDialect>();
+
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return cir::createConvertMLIRToLLVMPass();
+  });
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return mlir::createMergeCleanupsPass();
+  });
+
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return cir::createConvertCIRToMLIRPass();
+  });
+
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return cir::direct::createConvertCIRToLLVMPass();
+  });
+
+  ::mlir::registerPass([]() -> std::unique_ptr<::mlir::Pass> {
+    return mlir::createReconcileUnrealizedCastsPass();
+  });
+
+  mlir::registerTransformsPasses();
+
+  return failed(MlirOptMain(
+      argc, argv, "ClangIR-based cross-library optimization tool\n", registry));
+}
