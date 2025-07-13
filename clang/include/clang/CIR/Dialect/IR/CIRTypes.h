llvm/clangir

[CIR][IR][NFC] Redefine tablegen CIR StructType with C++
Essentially, this patch redefines the CIR StructType manually instead
of using the autogenerated definition from tablegen. This is the first
step to make StructType mutable, as this feature is not yet supported
by tablegen.

It's mostly a copy of the tablegen definition, with a few notable
differences:

 - A few embellishments are added to make the code more dev-friendly
 - Addition of a CIRTypesDetails.h file to keep custom storage definitions
 - The CIR_AnyCIRType constraint is removed, as it is not used and must
   be defined in C++ to ensure StructType is a part of it.

ghstack-source-id: 5f706dc0a61a4a2ed6e2f20ab0937b1a42bfa9cc
Pull Request resolved: https://github.com/llvm/clangir/pull/302

clang/include/clang/CIR/Dialect/IR/CIRTypes.h
@@ -20,10 +20,127 @@
 #include "clang/CIR/Interfaces/ASTAttrInterfaces.h"
 
 //===----------------------------------------------------------------------===//
-// CIR Dialect Types
+// CIR Dialect Tablegen'd Types
 //===----------------------------------------------------------------------===//
 
 #define GET_TYPEDEF_CLASSES
 #include "clang/CIR/Dialect/IR/CIROpsTypes.h.inc"
+
+//===----------------------------------------------------------------------===//
+// CIR StructType
+//
+// The base type for all RecordDecls.
+//===----------------------------------------------------------------------===//
+
+namespace mlir {
+namespace cir {
+
+namespace detail {
+struct StructTypeStorage;
+} // namespace detail
+
+/// Each unique clang::RecordDecl is mapped to a `cir.struct` and any object in
+/// C/C++ that has a struct type will have a `cir.struct` in CIR.
+class StructType
+    : public Type::TypeBase<StructType, Type, detail::StructTypeStorage,
+                            DataLayoutTypeInterface::Trait> {
+  // FIXME(cir): migrate this type to Tablegen once mutable types are supported.
+public:
+  using Base::Base;
+  using Base::getChecked;
+  using Base::verify;
+
+  static constexpr StringLiteral name = "cir.struct";
+
+  enum RecordKind : uint32_t { Class, Union, Struct };
+
+  /// Create a identified and complete struct type.
+  static StructType get(MLIRContext *context, ArrayRef<Type> members,
+                        StringAttr name, bool packed, RecordKind kind,
+                        ASTRecordDeclInterface ast = {});
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, ArrayRef<Type> members,
+                               StringAttr name, bool packed, RecordKind kind,
+                               ASTRecordDeclInterface ast = {});
+
+  /// Create a identified and incomplete struct type.
+  static StructType get(MLIRContext *context, StringAttr name, RecordKind kind);
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, StringAttr name,
+                               RecordKind kind);
+
+  /// Create a anonymous struct type (always complete).
+  static StructType get(MLIRContext *context, ArrayRef<Type> members,
+                        bool packed, RecordKind kind,
+                        ASTRecordDeclInterface ast = {});
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, ArrayRef<Type> members,
+                               bool packed, RecordKind kind,
+                               ASTRecordDeclInterface ast = {});
+
+  /// Validate the struct about to be constructed.
+  static LogicalResult verify(function_ref<InFlightDiagnostic()> emitError,
+                              ArrayRef<Type> members, StringAttr name,
+                              bool incomplete, bool packed,
+                              StructType::RecordKind kind,
+                              ASTRecordDeclInterface ast);
+
+  // Parse/print methods.
+  static constexpr StringLiteral getMnemonic() { return {"struct"}; }
+  static Type parse(AsmParser &odsParser);
+  void print(AsmPrinter &odsPrinter) const;
+
+  // Accessors
+  ASTRecordDeclInterface getAst() const;
+  ArrayRef<Type> getMembers() const;
+  StringAttr getName() const;
+  StructType::RecordKind getKind() const;
+  bool getIncomplete() const;
+  bool getPacked() const;
+  void dropAst();
+
+  // Predicates
+  bool isClass() const { return getKind() == RecordKind::Class; };
+  bool isStruct() const { return getKind() == RecordKind::Struct; };
+  bool isUnion() const { return getKind() == RecordKind::Union; };
+  bool isComplete() const { return !isIncomplete(); };
+  bool isIncomplete() const;
+
+  // Utilities
+  Type getLargestMember(const DataLayout &dataLayout) const;
+  size_t getNumElements() const { return getMembers().size(); };
+  std::string getKindAsStr() {
+    switch (getKind()) {
+    case RecordKind::Class:
+      return "class";
+    case RecordKind::Union:
+      return "union";
+    case RecordKind::Struct:
+      return "struct";
+    }
+  }
+  std::string getPrefixedName() {
+    return getKindAsStr() + "." + getName().getValue().str();
+  }
+
+  /// DataLayoutTypeInterface methods.
+  llvm::TypeSize getTypeSizeInBits(const DataLayout &dataLayout,
+                                   DataLayoutEntryListRef params) const;
+  uint64_t getABIAlignment(const DataLayout &dataLayout,
+                           DataLayoutEntryListRef params) const;
+  uint64_t getPreferredAlignment(const DataLayout &dataLayout,
+                                 DataLayoutEntryListRef params) const;
+
+  // Utilities for lazily computing and cacheing data layout info.
+private:
+  mutable Type largestMember{};
+  mutable std::optional<bool> padded{};
+  mutable std::optional<unsigned> size{}, align{};
+  bool isPadded(const DataLayout &dataLayout) const;
+  void computeSizeAndAlignment(const DataLayout &dataLayout) const;
+};
+
+} // namespace cir
+} // namespace mlir
 
 #endif // MLIR_DIALECT_CIR_IR_CIRTYPES_H_

lanza/llvm-project

clang/include/clang/CIR/Dialect/IR/CIRTypes.h
@@ -20,10 +20,125 @@
 #include "clang/CIR/Interfaces/ASTAttrInterfaces.h"
 
 //===----------------------------------------------------------------------===//
-// CIR Dialect Types
+// CIR Dialect Tablegen'd Types
 //===----------------------------------------------------------------------===//
 
 #define GET_TYPEDEF_CLASSES
 #include "clang/CIR/Dialect/IR/CIROpsTypes.h.inc"
+
+//===----------------------------------------------------------------------===//
+// CIR StructType
+//
+// The base type for all RecordDecls.
+//===----------------------------------------------------------------------===//
+
+namespace mlir {
+namespace cir {
+
+namespace detail {
+struct StructTypeStorage;
+} // namespace detail
+
+/// Each unique clang::RecordDecl is mapped to a `cir.struct` and any object in
+/// C/C++ that has a struct type will have a `cir.struct` in CIR.
+class StructType
+    : public Type::TypeBase<StructType, Type, detail::StructTypeStorage,
+                            DataLayoutTypeInterface::Trait> {
+  // FIXME(cir): migrate this type to Tablegen once mutable types are supported.
+public:
+  using Base::Base;
+  using Base::getChecked;
+  using Base::verifyInvariants;
+
+  static constexpr StringLiteral name = "cir.struct";
+
+  enum RecordKind : uint32_t { Class, Union, Struct };
+
+  /// Create a identified and complete struct type.
+  static StructType get(MLIRContext *context, ArrayRef<Type> members,
+                        StringAttr name, bool packed, RecordKind kind,
+                        ASTRecordDeclInterface ast = {});
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, ArrayRef<Type> members,
+                               StringAttr name, bool packed, RecordKind kind,
+                               ASTRecordDeclInterface ast = {});
+
+  /// Create a identified and incomplete struct type.
+  static StructType get(MLIRContext *context, StringAttr name, RecordKind kind);
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, StringAttr name,
+                               RecordKind kind);
+
+  /// Create a anonymous struct type (always complete).
+  static StructType get(MLIRContext *context, ArrayRef<Type> members,
+                        bool packed, RecordKind kind,
+                        ASTRecordDeclInterface ast = {});
+  static StructType getChecked(function_ref<InFlightDiagnostic()> emitError,
+                               MLIRContext *context, ArrayRef<Type> members,
+                               bool packed, RecordKind kind,
+                               ASTRecordDeclInterface ast = {});
+
+  /// Validate the struct about to be constructed.
+  static LogicalResult
+  verifyInvariants(function_ref<InFlightDiagnostic()> emitError,
+                   ArrayRef<Type> members, StringAttr name, bool incomplete,
+                   bool packed, StructType::RecordKind kind,
+                   ASTRecordDeclInterface ast);
+
+  // Parse/print methods.
+  static constexpr StringLiteral getMnemonic() { return {"struct"}; }
+  static Type parse(AsmParser &odsParser);
+  void print(AsmPrinter &odsPrinter) const;
+
+  // Accessors
+  ASTRecordDeclInterface getAst() const;
+  ArrayRef<Type> getMembers() const;
+  StringAttr getName() const;
+  StructType::RecordKind getKind() const;
+  bool getIncomplete() const;
+  bool getPacked() const;
+  void dropAst();
+
+  // Predicates
+  bool isClass() const { return getKind() == RecordKind::Class; };
+  bool isStruct() const { return getKind() == RecordKind::Struct; };
+  bool isUnion() const { return getKind() == RecordKind::Union; };
+  bool isComplete() const { return !isIncomplete(); };
+  bool isIncomplete() const;
+
+  // Utilities
+  Type getLargestMember(const DataLayout &dataLayout) const;
+  size_t getNumElements() const { return getMembers().size(); };
+  std::string getKindAsStr() {
+    switch (getKind()) {
+    case RecordKind::Class:
+      return "class";
+    case RecordKind::Union:
+      return "union";
+    case RecordKind::Struct:
+      return "struct";
+    }
+  }
+  std::string getPrefixedName() {
+    return getKindAsStr() + "." + getName().getValue().str();
+  }
+
+  /// DataLayoutTypeInterface methods.
+  llvm::TypeSize getTypeSizeInBits(const DataLayout &dataLayout,
+                                   DataLayoutEntryListRef params) const;
+  uint64_t getABIAlignment(const DataLayout &dataLayout,
+                           DataLayoutEntryListRef params) const;
+
+  // Utilities for lazily computing and cacheing data layout info.
+private:
+  mutable Type largestMember{};
+  mutable std::optional<bool> padded{};
+  mutable std::optional<unsigned> size{}, align{};
+  bool isPadded(const DataLayout &dataLayout) const;
+  void computeSizeAndAlignment(const DataLayout &dataLayout) const;
+};
+
+} // namespace cir
+} // namespace mlir
 
 #endif // MLIR_DIALECT_CIR_IR_CIRTYPES_H_