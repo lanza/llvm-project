llvm/clangir

[CIR] Add initial support for idiom recognizing std::find
- Identify such calls and produce a remark. Next step is to map
this to a CIR operation representing std::find, which should
come next.
- Add new `-fclangir-idiom-recognizer=` option, used to control
remark options for now.

clang/lib/Frontend/CompilerInvocation.cpp
@@ -2925,6 +2925,10 @@ static bool ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
     Opts.ClangIRLifetimeCheckOpts = A->getValue();
   }
 
+  if (Args.hasArg(OPT_fclangir_idiom_recognizer_EQ))
+    Opts.AuxTargetCPU =
+        std::string(Args.getLastArgValue(OPT_fclangir_idiom_recognizer_EQ));
+
   if (Args.hasArg(OPT_aux_target_cpu))
     Opts.AuxTargetCPU = std::string(Args.getLastArgValue(OPT_aux_target_cpu));
   if (Args.hasArg(OPT_aux_target_feature))

lanza/llvm-project

clang/lib/Frontend/CompilerInvocation.cpp
@@ -3139,6 +3139,9 @@ static bool ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
     Opts.ClangIRLifetimeCheck = true;
     Opts.ClangIRLifetimeCheckOpts = A->getValue();
   }
+  if (Args.hasArg(OPT_fclangir_idiom_recognizer_EQ))
+    Opts.ClangIRIdiomRecognizerOpts =
+        std::string(Args.getLastArgValue(OPT_fclangir_idiom_recognizer_EQ));
   if (Args.hasArg(OPT_aux_target_cpu))
     Opts.AuxTargetCPU = std::string(Args.getLastArgValue(OPT_aux_target_cpu));
   if (Args.hasArg(OPT_aux_target_feature))