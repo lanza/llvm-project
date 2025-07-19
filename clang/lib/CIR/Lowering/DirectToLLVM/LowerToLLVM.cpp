llvm/clangir

[CIR][CIRGen][Builtin][Type] Support for IEEE Quad (long double) added (in CIR + Direct to LLVM) (#966)
Fixes https://github.com/llvm/clangir/issues/931
Added type definition in CIRTypes.td, created appropriate functions for
the same in CIRTypes.cpp like getPreferredAlignment,
getPreferredAlignment, etc. Optionally added lowering in LowerToLLVM.cpp

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -4210,6 +4210,9 @@ void prepareTypeConverter(mlir::LLVMTypeConverter &converter,
   converter.addConversion([&](mlir::cir::FP80Type type) -> mlir::Type {
     return mlir::FloatType::getF80(type.getContext());
   });
+  converter.addConversion([&](mlir::cir::FP128Type type) -> mlir::Type {
+    return mlir::FloatType::getF128(type.getContext());
+  });
   converter.addConversion([&](mlir::cir::LongDoubleType type) -> mlir::Type {
     return converter.convertType(type.getUnderlying());
   });

lanza/llvm-project

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -4200,6 +4200,9 @@ void prepareTypeConverter(mlir::LLVMTypeConverter &converter,
   converter.addConversion([&](mlir::cir::FP80Type type) -> mlir::Type {
     return mlir::Float80Type::get(type.getContext());
   });
+  converter.addConversion([&](mlir::cir::FP128Type type) -> mlir::Type {
+    return mlir::Float128Type::get(type.getContext());
+  });
   converter.addConversion([&](mlir::cir::LongDoubleType type) -> mlir::Type {
     return converter.convertType(type.getUnderlying());
   });